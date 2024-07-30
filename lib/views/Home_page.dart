6cafc6056f9d5b7497af9d6d72c2df35f494792f
dhruv-bhalani <bhlanidhruv10@gmail.com>
2024-07-30 16:20:24 +0530

    flutter

--- lib/views/Home_page.dart
+++ lib/views/Home_page.dart
@@ -1,3 +1,4 @@
+import 'package:analog_clock/analog_clock.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,12 +11,14 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
+
     return Scaffold(
-      drawer: const Drawer(
+      drawer: Drawer(
         backgroundColor: Colors.white,
         child: Column(
           children: [
-            UserAccountsDrawerHeader(
+            const UserAccountsDrawerHeader(
               accountName: Text("Dhruv Bhalani"),
               accountEmail: Text("Dhruvbhalani9@.gmail"),
               decoration: BoxDecoration(color: Colors.red),
@@ -24,14 +27,36 @@ class _HomePageState extends State<HomePage> {
                     "https://avatars.githubusercontent.com/u/156923442?s=400&u=8b2e6cf085f2185acbd956c2d57714c15f80d74e&v=4"),
               ),
             ),
+            const Text(
+              "Clocks",
+              selectionColor: Colors.black,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                Image.network(
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRmy9tqJR9bUTjT-fEEzVk4txB01RRKHG1Mw&s"
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWYzGfZUrsi8J-sPK5EwwuPUVcGGBgyBWhPQ&s"),
+              ],
+            ),
           ],
         ),
       ),
       appBar: AppBar(
         title: Text("Home Page"),
       ),
-      body: Padding(
+      body: const Padding(
         padding: EdgeInsets.all(16),
+        child: Center(
+          child: AnalogClock(
+            digitalClockColor: Colors.black,
+            height: 500,
+            numberColor: Colors.blue,
+            tickColor: Colors.red,
+          ),
+        ),
         // child:
       ),
       floatingActionButton: FloatingActionButton(
--- lib/views/clock_page.dart
+++ lib/views/clock_page.dart
@@ -10,8 +10,30 @@ class clockpage extends StatefulWidget {
 }
 
 class _clockpageState extends State<clockpage> {
+  int h = 0, s = 0, m = 0;
+  void clock() {
+    s = DateTime.now().second;
+    m = DateTime.now().minute;
+    h = DateTime.now().hour;
+    setState(() {});
+    Future.delayed(const Duration(seconds: 1), () {
+      clock();
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    clock();
+    super.initState();
+  }
+
+  bool _isAnalog = false;
+  bool _isStrap = true;
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Clock Page"),
@@ -26,47 +48,98 @@ class _clockpageState extends State<clockpage> {
         ],
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16),
+        padding: EdgeInsets.all(16),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Center(
-              child: Transform.scale(
-                scaleX: 5,
-                child: const CircularProgressIndicator(
-                  value: 1,
+            Visibility(
+              visible: _isStrap,
+              child: Center(
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Transform.scale(
+                      scale: 6,
+                      child: CircularProgressIndicator(
+                        strokeWidth: 2,
+                        value: (h % 12) / 12,
+                        color: Colors.orange,
+                      ),
+                    ),
+                    Transform.scale(
+                      scale: 5,
+                      child: CircularProgressIndicator(
+                        strokeWidth: 2,
+                        value: m / 60,
+                        color: Colors.red,
+                      ),
+                    ),
+                    Transform.scale(
+                      scale: 4,
+                      child: CircularProgressIndicator(
+                        strokeWidth: 2,
+                        value: s / 60,
+                        color: Colors.green,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
-            Transform.rotate(
-              angle: pi / 2,
-              child: Divider(
-                color: Colors.black,
-                indent: 100,
-                endIndent: 50,
+            Visibility(
+              visible: _isAnalog,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    height: 300,
+                    width: 300,
+                    child: Image.network(
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg-oTXjhnNIh7HNSWTHeeNy3Lpq0yhwckvDA&s",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Transform.rotate(
+                    angle: h * (pi * 2) / 12,
+                    child: Transform.rotate(
+                      angle: pi / 2,
+                      child: Divider(
+                        color: Colors.black,
+                        thickness: 7,
+                        indent: 120,
+                        endIndent: size.width * 0.5 - 16,
+                      ),
+                    ),
+                  ),
+                  Transform.rotate(
+                    angle: m * (pi * 2) / 60,
+                    child: Transform.rotate(
+                      angle: pi / 2,
+                      child: Divider(
+                        color: Colors.black,
+                        thickness: 5,
+                        indent: 95,
+                        endIndent: size.width * 0.5 - 16,
+                      ),
+                    ),
+                  ),
+                  Transform.rotate(
+                    angle: s * (pi * 2) / 60,
+                    child: Transform.rotate(
+                      angle: pi / 3,
+                      child: Divider(
+                        color: Colors.red,
+                        thickness: 2,
+                        indent: 70,
+                        endIndent: size.width * 0.5 - 16,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            SizedBox(
-              height: 50,
-            ),
-            Transform.translate(
-                offset: Offset(0, 0), child: CircularProgressIndicator())
           ],
         ),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          FloatingActionButton(
-            child: const Icon(Icons.rotate_left),
-            onPressed: () {},
-          ),
-          const SizedBox(
-            width: 10,
-          ),
-          FloatingActionButton(
-              child: const Icon(Icons.rotate_right), onPressed: () {}),
-        ],
       ),
     );
   }
